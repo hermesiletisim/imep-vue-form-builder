bab074713816b7fef813fb68a1b75dc8e29df056
Guney Kaya <guney.kaya@hermesiletisim.net>
2024-10-03 15:02:34 +0300

    userControl control

--- src/views/controls/UserControl.vue
+++ src/views/controls/UserControl.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="person-input">
-
+        {{ control }}
         <input :id="control.uniqueId"
            :type="control.typeAttribute"
            :class="controlFieldClass"
